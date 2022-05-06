26383dc3536c23ce85f8e81773d111c7540f2da0
Ram <ss4033167@gmail.com>
2022-05-06 23:11:18 +0530

    complete cart page

--- components/common.js
+++ components/common.js
@@ -1,7 +1,7 @@
 let quantity_div=()=>{
     return `
     <div id="minus">-</div>
-    <div id="r_in"><input id="r_number"type="text" /></div>
+    <div id="r_in"><input id="r_number" type="text" /></div>
     <div id="plus">+</div>
 `
 };
--- scripts/cart.js
+++ scripts/cart.js
@@ -32,9 +32,10 @@ function getEl(id){
 // collect data from local storage and append the data id r_data
 let data= JSON.parse(localStorage.getItem("cart"))
 // console.log(data)
+let total_price=0;
 function appendData(data) {
     console.log(data)
-    data.forEach(({Poster,Title,image,name,price,quantity},index)=>{
+    data.forEach(({image,name,price,quantity},index)=>{
         console.log(index)
         let main=document.createElement("div")
 
@@ -58,23 +59,108 @@ function appendData(data) {
         pPrice.innerText=price;
         let col4=document.createElement("div")
         col4.className="r_inQuntity";
-        col4.innerHTML=quantity_div();
+        // col4.innerHTML=quantity_div();
+        let dec=document.createElement("div")
+        let qunt=document.createElement("div")
+        let inc=document.createElement("div")
+        dec.id="minus"
+        dec.innerText="-"
+        qunt.id="r_in"
+        inc.id="plus"
+        inc.innerText="+"
+        let r_inp= document.createElement("input")
+        r_inp.type="text"
+        r_inp.id="r_number"
+        r_inp.value=5
+        dec.addEventListener("click",()=>{
+            // console.log(r_inp.value)
+            r_inp.value--
+            col5.innerText=price*r_inp.value;
+            total_price-=price
+            document.getElementById("pp").innerText=total_price
+            if(Number(r_inp.value)==0){
+                remove(data,index)
+            }
+
+        });
+        inc.addEventListener("click",()=>{
+            // console.log(r_inp.value)
+            r_inp.value++
+            col5.innerText=price*r_inp.value;
+            total_price+=price
+            document.getElementById("pp").innerText=total_price
+            if(Number(r_inp.value)==0){
+                remove(data,index)
+            }
+
+        });
+        r_inp.addEventListener("input",()=>{
+            col5.innerText=price*r_inp.value;
+            total_price=ram_total(document.querySelectorAll("#r_col5"))
+            document.getElementById("pp").innerText=total_price
+            if(Number(r_inp.value)==0){
+                remove(data,index)
+            }
+
+
+        });
+
+        qunt.append(r_inp)
+        col4.append(dec,qunt,inc)
+
         let col5=document.createElement("p")
-        col5.innerText=pPrice*col4;
-
+        col5.id="r_col5"
+        col5.innerText=price*r_inp.value;
+        //collect from local storage
+        total_price+=Number(col5.innerText)
+        document.getElementById("pp").innerText=total_price
+        let hr=document.createElement("hr")
         main.append(col1,pName,pPrice,col4,col5)
-        document.getElementById("r_data").append(main)
+        document.getElementById("r_data").append(main,hr)
 
 
-
+        // console.log(document.querySelectorAll("#r_col5"))
 
 
 
 
     })
 
-}
+};
+
 function remove(data,index){
     console.log(index)
+    data.splice(index,1)
+    localStorage.setItem("key_",JSON.stringify(data))
 }
 appendData(data)
+// let quantity=JSON.parse(localStorage.getItem("qnt")) || 0
+// let minuS=document.querySelectorAll("#minus")
+// for(let x of minuS){
+//     console.log(x)
+//     x.addEventListener("click",()=>{
+//         quantity--;
+//         console.log(x.id,quantity)
+//     })
+// }
+
+// document.getElementById("r_number").addEventListener("click",()=>{
+//     quantity++;
+//     // console.log(quantity)
+// })
+
+function ram_total(data){
+    let total=0
+    for(let i=1; i<data.length; i++){
+        total+=Number(data[i].innerText)
+    }
+    return total
+    // console.log(total)
+}
+function update(){
+    window.location.reload()
+}
+function proceedFun() {
+    localStorage.setItem("subtotal",total_price)
+    window.location.href="proceed page"// connect with next page checkout page
+}
